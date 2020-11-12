294cbd6acf13951921e9dbf836817128c973a987
James Newton-King <james@newtonking.com>
2020-11-13 09:33:31 +1300

    Fix build warnings

--- Server/Controllers/WeatherForecastController.cs
+++ Server/Controllers/WeatherForecastController.cs
@@ -49,6 +49,6 @@ namespace Server.Controllers
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
